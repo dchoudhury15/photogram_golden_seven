893b5d40720d1898e33853d1f02a2524299f5fab
Diganto <digantochoudhury@gmail.com>
2017-04-25 09:40:13 -0500

    update 1

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,13 @@
+class PhotosController < ApplicationController
+  def show
+    # the params hash looks like this: {"the_id_number" => "1"}
+
+    id_number=params["id_num"]
+    p=Photo.find(id_number)
+
+    @the_caption=p.caption
+    @the_image_url=p.source
+    @last_updated=p.created_at
+    render("photos/show.html.erb")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+
+  get("/photos/:id_num", {:controller => "photos", :action => "show"})
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
